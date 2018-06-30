66276ff8c0f716c0238ab24fa6bfc2411e34d89e
Xuefei Cao <xcstf01@gmail.com>
2018-06-30 12:13:12 -0400

    add doc to data_prepare

--- tdcca/data_preprocess.py
+++ tdcca/data_preprocess.py
@@ -6,14 +6,27 @@ from six.moves import cPickle as pickle
 from scipy.io import savemat 
 
 #to be done: how to select the tol 
-def data_prepare(data_X, folder_name, nu, pre_sign=False, same_cut=True, add_class=True):
+def data_prepare(data_X, folder_name, nu, pre_sign=True, same_cut=True, add_class=True):
     """
     preprocess the data X and save the processed data ('data/folder_name/*')
+
+    Parameters
+    ----------------------
+    data_X: class of tcca_config
+    folder_name: path for saving the data
+    pre_sign: boolean, default True.  
+             solving sign issue, first try 
+    same_cut: whether select the same number of svd vectors for all time points. In other words, 
+              whether the dimension of \Sigma should be same along time dimension 
+    add_class: whether add to class attr
+
+    Returns
+    ----------------------
+    None 
+
     Saved: SVD_x[(i,t)] = (Q_{1it}, \Sigma_it^{-1}, U_{1it}); SVD_ijt[(i,j,t)] = (P_it, P_jt);
     UTU[(i,t)] = U_{1it}U_{1it}^T ; US[(i,t)] = U_{1it}\Sigma_{it}^{-1}; P[(i,t)] = \sum_{i<j} P_it^{(i,j)} + \sum_{w<i} P_{it}^{(w,i)}
-    @param : data_X(class of tcca_config), folder_name (path for saving the data), tol (eigenvalue selection), add_class(whether add to class attr)
     
-    @Return: (n,d_i,T)
     """
 
     X = data_X.X
@@ -29,58 +42,20 @@ def data_prepare(data_X, folder_name, nu, pre_sign=False, same_cut=True, add_cla
     P = dict()
     SVD_ijt = dict()
     #real data
-    if 'test_para_1' in folder_name:
-        tol = 0.8
-    else:
-        tol = 0.9
 
     if not os.path.exists(folder_name):
         os.mkdir(folder_name)
-    print tol
+
     # changing this to parallel
     for i in range(N):
-        
-    #     for t in range(T):
-
-    #         tmp = X[i][:,:,t]
-    #         Q_it, s_it, U_itT = np.linalg.svd(tmp, full_matrices=0)
-
-    #         cut_ind = search_ind(s_it, tol)
-    #         #print s_it, cut_ind
-    #         if '223/' in folder_name:
-    #             cut_ind = 2
-    #         elif '223_1/' in folder_name:
-    #             cut_ind = 3
-    #         else:
-    #             pass
-    #         Q_1it = Q_it[:,:cut_ind]
-    #         S_it = np.diag(1.0/s_it[:cut_ind]) # store the inversed matrix 
-    #         U_1it = np.transpose(U_itT)[:,:cut_ind]
-    #         print t, i, Q_1it.shape, U_1it.shape
-
-    #         ind = np.ravel_multi_index((i,t),(N,T))
-    #         if pre_sign:
-
-    #             if t >= 1:
-    #                 cut_ind_0 = SVD_x[ind-1][2].shape[1]
-    #                 tmp_cut = min(cut_ind, cut_ind_0)
-                
-    #                 if np.sum(abs(U_1it[:,0] - SVD_x[ind-1][2][:,0])) > np.sum(abs(-U_1it[:,0]-SVD_x[ind-1][2][:,0])):
-    #                     #print U_1it, SVD_x[ind-1][2]
-    #                     U_1it = - U_1it
-    #                     Q_1it = - Q_1it
-
-    #         SVD_x[ind] = [Q_1it, S_it, U_1it]
-    #         UTU[ind] = np.dot(U_1it, np.transpose(U_1it))
-    #         US[ind] = np.dot(U_1it, S_it)
         cc = []
-        for t in range(T):
-            tmp = X[i][:,:,t]
-            Q_it, s_it, U_itT = np.linalg.svd(tmp, full_matrices=0)
-            cut_ind = search_ind(s_it, tol)
-            cc.append(cut_ind)
-        cut_ind = int(np.mean(cc))
-        print(cut_ind)
+        if same_cut:
+            for t in range(T):
+                tmp = X[i][:,:,t]
+                Q_it, s_it, U_itT = np.linalg.svd(tmp, full_matrices=0)
+                cut_ind = search_ind(s_it, tol)
+                cc.append(cut_ind)
+            cut_ind = int(np.mean(cc))
 
         for t in range(T):
             tmp = X[i][:,:,t]
@@ -98,7 +73,6 @@ def data_prepare(data_X, folder_name, nu, pre_sign=False, same_cut=True, add_cla
 
                 if t >= 1:
                     cut_ind_0 = SVD_x[ind-1][2].shape[1]
-                    tmp_cut = min(cut_ind, cut_ind_0)
                 
                     if np.sum(abs(U_1it[:,0] - SVD_x[ind-1][2][:,0])) > np.sum(abs(-U_1it[:,0]-SVD_x[ind-1][2][:,0])):
                         #print U_1it, SVD_x[ind-1][2]
@@ -120,7 +94,6 @@ def data_prepare(data_X, folder_name, nu, pre_sign=False, same_cut=True, add_cla
                 P_i, _, P_jT = np.linalg.svd(tmp, full_matrices=1)
                 P_j = P_jT.T
                 SVD_ijt[np.ravel_multi_index((i,j,t),(N,N,T))] = (P_i[:,0:l].reshape((-1,l)), P_j[:,0:l].reshape((-1,l)))
-                #SVD_ijt[np.ravel_multi_index((i,j,t),(N,N,T))] = (P_i, P_j)
     
 
     for t in range(T):
@@ -162,14 +135,8 @@ def data_prepare(data_X, folder_name, nu, pre_sign=False, same_cut=True, add_cla
                 'data': [X, X_test],
                 }
             pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
-        #np.save(folder_name + 'data_1.npy', X[0])
-        #np.save(folder_name + 'data_2.npy', X[1])
-
-        #if len(X_test) > 1:
-        #    np.save(folder_name + 'data_test_1.npy', X_test[0])
-        #    np.save(folder_name + 'data_test_2.npy', X_test[1])
-            
-            
+ 
+                       
 
     # decide to save this to h, remove this afterwards
     if sys.getsizeof(UTU[0]) < 0.5 * 1e6 and False:
@@ -195,11 +162,7 @@ def data_prepare(data_X, folder_name, nu, pre_sign=False, same_cut=True, add_cla
                  
                             f[str(i)+'/' + str(t)] = tmp
         UTU = [] 
-        # with h5py.File(folder_name + 'svd_all.hdf5', 'w') as f:
-        #     f['SVD_x'] = SVD_x
-        #     f['SVD_ijt'] = SVD_ijt
-        #     f['US'] = US
-        #     f['P'] = P 
+
         with open(folder_name + 'SVD_x.pkl', 'wb') as f:
             pickle.dump(SVD_x, f, pickle.HIGHEST_PROTOCOL)
 
@@ -222,11 +185,6 @@ def data_prepare(data_X, folder_name, nu, pre_sign=False, same_cut=True, add_cla
         save_dict_to_hdf5(P, folder_name +'P.hdf5')
         
 
-        #data_X.UTU = UTU
-        #data_X.US = US
-        #data_X.P = P 
-        #data_X.SVD_x = SVD_x
-        #data_X.SVD_ijt = SVD_ijt
     del data_X.X
     del data_X.X_test
 
@@ -237,7 +195,6 @@ def search_ind(l, tol = 0.95):
     """
     search a descending order list and return the first index whose value is less than or equal to tol
     """
-    #return 1
     l = (l**2).tolist()
     ss = np.sum(l)
     s = 0
